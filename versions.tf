603447fbc2bb7d3e633b6a84f0a456e214fe604f
Scott Winkler <Scott.Winkler@elliemae.com>
2021-01-02 21:26:00 -0800

    update

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
-/*terraform {
-  required_version = "~> 0.12"
+terraform {
+  required_version = "~> 0.14"
   required_providers {
-    aws = "~> 2.19"
-    random = "~> 2.1"
+    aws = "~> 3.22"
+    random = "~> 3.0"
   }
-}*/
+}
